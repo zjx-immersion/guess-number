a3e70a58f2d7d2134afe9e282d4f43bf54853ac9
钟健鑫 <zhongjianxin@zjx.local>
2019-10-13 00:37:17 +0800

    [Jason] initial a basic coding project

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    private Dependency dependency;
+
+    public HelloWorld(Dependency dependency) {
+
+        this.dependency = dependency;
+    }
+    public String beenCalled() {
+        return dependency.say();
+    }
+}
