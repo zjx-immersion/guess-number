6e75c392204bd6812a2d8edbde33b86545b35070
钟健鑫 <zhongjianxin@zjx.local>
2019-10-14 17:17:00 +0800

    [Jason] pass the sencd test case should_get_the_fail_status_when_guess_action_count_over_or_equal_6

--- src/main/java/com/hw/domain/Game.java
+++ src/main/java/com/hw/domain/Game.java
@@ -1,17 +1,34 @@
 package com.hw.domain;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
 
     private final AnswerGenerator answerGeneratorStub;
     private final Answer originalAnswer;
+    private final List<String> guessResults;
 
     public Game(AnswerGenerator AnswerGenerator) {
         this.answerGeneratorStub = AnswerGenerator;
         this.originalAnswer = answerGeneratorStub.run();
+        this.guessResults = new ArrayList<>();
     }
 
     public String guess(String userAnswer) {
-        return this.originalAnswer.check(userAnswer);
+        String guessResult = this.originalAnswer.check(userAnswer);
+        this.guessResults.add(guessResult);
+        return guessResult;
+    }
 
+    public String checkStatus() {
+        boolean isSuccess = guessResults.stream().anyMatch(result -> result.equals("4A0B"));
+        if (isSuccess) {
+            return "success";
+        } else {
+            return "fail";
+        }
     }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -16,38 +16,43 @@ public class GameTest {
         Game game = new Game(answerGeneratorStub);
         String userAnswer = "1 2 3 4";
         String expectedGuessResult = "4A0B";
+        String expectedStatus = "success";
 
         // when
         String result = game.guess(userAnswer);
+        String statusOfGame = game.checkStatus();
 
         // then
         Assert.assertEquals(expectedGuessResult, result);
+        Assert.assertEquals(expectedStatus, statusOfGame);
 
     }
 
-//    @Test
-//    public void should_get_the_fail_status_when_guess_action_count_over_or_equal_6() {
-//        // given
-//        Answer answer = new Answer("1 2 3 4");
-//        AnswerGenerator answerGeneratorStub = new AnswerGeneratorStub(answer);
-//        Game game = new Game(answerGeneratorStub);
-//        String userAnswer1 = "4 3 2 1";
-//        String userAnswer2 = "4 3 2 1";
-//        String userAnswer3 = "4 3 2 1";
-//        String userAnswer4 = "4 3 2 1";
-//        String userAnswer5 = "4 3 2 1";
-//        String userAnswer6 = "4 3 2 1";
-//
-//        // when
-//        String result1 = game.guess(userAnswer1);
-//        String result2 = game.guess(userAnswer2);
-//        String result3 = game.guess(userAnswer3);
-//        String result4 = game.guess(userAnswer4);
-//        String result5 = game.guess(userAnswer5);
-//        String result6 = game.guess(userAnswer6);
-//
-//        // then
-//        Assert.assertEquals("success", result); // ??
-//
-//    }
+    @Test
+    public void should_get_the_fail_status_when_guess_action_count_over_or_equal_6() {
+        // given
+        Answer answer = new Answer("1 2 3 4");
+        AnswerGenerator answerGeneratorStub = new AnswerGeneratorStub(answer);
+        Game game = new Game(answerGeneratorStub);
+        String userAnswer1 = "4 3 2 1";
+        String userAnswer2 = "4 3 2 1";
+        String userAnswer3 = "4 3 2 1";
+        String userAnswer4 = "4 3 2 1";
+        String userAnswer5 = "4 3 2 1";
+        String userAnswer6 = "4 3 2 1";
+        String expectStatusOfGame = "fail";
+
+        // when
+        String result1 = game.guess(userAnswer1);
+        String result2 = game.guess(userAnswer2);
+        String result3 = game.guess(userAnswer3);
+        String result4 = game.guess(userAnswer4);
+        String result5 = game.guess(userAnswer5);
+        String result6 = game.guess(userAnswer6);
+        String statusOfGame = game.checkStatus();
+
+        // then
+        Assert.assertEquals(expectStatusOfGame, statusOfGame); // ??
+
+    }
 }
